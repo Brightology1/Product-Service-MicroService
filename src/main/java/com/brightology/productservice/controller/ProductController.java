41cb57a05dd69b47e89055ab9cca312a34f7011e
HP 250g7 <brightology2@gmain.com>
2023-05-15 18:23:50 +0100

    Product Response update

--- src/main/java/com/brightology/productservice/controller/ProductController.java
+++ src/main/java/com/brightology/productservice/controller/ProductController.java
@@ -1,17 +1,32 @@
 package com.brightology.productservice.controller;
 
 import com.brightology.productservice.dto.ProductRequest;
+import com.brightology.productservice.dto.ProductResponse;
 import com.brightology.productservice.model.Product;
+import com.brightology.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
 
     }
 }
--- src/main/java/com/brightology/productservice/service/ProductService.java
+++ src/main/java/com/brightology/productservice/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.brightology.productservice.service;
 
 import com.brightology.productservice.dto.ProductRequest;
+import com.brightology.productservice.dto.ProductResponse;
 import com.brightology.productservice.model.Product;
 import com.brightology.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +25,22 @@ public class ProductService {
                 .build();
 
         productRepository.save(product);
-        log.info("Product " + product.getId() + " saved successfully");
+        log.info("Product {} saved successfully", product.getId());
     }
 
-    //
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
 }
